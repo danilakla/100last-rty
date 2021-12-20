0042aac68764034140114687d06712618f07c6b9
danila <danik.kla@mail.ru>
2021-12-20 17:20:16 +0300

    proto function

--- func.h
+++ func.h
@@ -1,5 +1,61 @@
 #pragma once
-class func
+#include "lib.h"
+#include "date.h"
+class func :protected date
 {
-};
+public:
+	//main manipulate method 
+	void creatNewPerson();
+	void print();
+	void del();
+	void addTofile();
+	int file();
+	void findthedate();
+	void  changTheData();
+	void sortDATE();
+private:
 
+	//find data
+	void findOnNAME();
+	void findOnFirstName();
+	void findOnsecName();
+	void findOnCountry();
+	void findOnCity();
+	void findOnStreet();
+	void findOnNumberOFhouse();
+	void findOnBirthday();
+	void findOnFaculty();
+	void findOnSpecialty();
+	void findOnCourse();
+	void findOnGroup();
+	void findOnAllParm();
+
+	void submenu();
+	int choice();
+	void infMenu();
+	void listForSort(int i);
+
+	//sort data
+
+	void SORTfindOnNAME();
+	void SORTfindOnFirstName();
+	void SORTfindOnsecName();
+	void SORTfindOnCountry();
+	void SORTfindOnCity();
+	void SORTfindOnStreet();
+	void SORTfindOnNumberOFhouse();
+	void SORTfindOnBirthday();
+	void SORTfindOnFaculty();
+	void SORTfindOnSpecialty();
+	void SORTfindOnCourse();
+	void SORTfindOnGroup();
+
+	void SORTsubmenu();
+	int SORTchoice();
+	void SORTinfMenu();
+
+	//kost;
+	void kosl();
+
+
+};
